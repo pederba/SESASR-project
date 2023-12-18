646b23b49aa65a8dfdc1dd3422eabbbede7ded24
pederba <peder.basmo@gmail.com>
2023-12-18 10:18:54 +0100

    update når odom gir måling istedenfor hvert ekf step

--- src/project_pkg/project_pkg/localization_node.py
+++ src/project_pkg/project_pkg/localization_node.py
@@ -17,14 +17,14 @@ class LocalizationNode(Node):
 
         self.__ground_truth_sub = self.create_subscription(
             Odometry,
-            'ground_truth',
+            '/ground_truth',
             self.ground_truth_callback,
             10)
         self.__ground_truth_sub
 
         self.__odom_sub = self.create_subscription(
             Odometry,
-            'odom',
+            '/diff_drive_controller/odom',
             self.odom_callback,
             10)
         self.__odom_sub
@@ -74,9 +74,9 @@ class LocalizationNode(Node):
         self.ekf.Mt = np.diag([self.std_rot1**2, self.std_transl**2, self.std_rot2**2])
         self.ekf.Qt = np.diag([self.std_rng**2, self.std_brg**2])
 
-        self.odom_pos = self.initial_pose
-        self.prev_pose = self.initial_pose
-        self.true_pose = self.initial_pose
+        self.odom_pose = self.initial_pose.copy()
+        self.prev_pose = self.initial_pose.copy()
+        self.true_pose = self.initial_pose.copy()
 
     def ground_truth_callback(self, msg):
         x_pose_true = msg.pose.pose.position.x
@@ -88,18 +88,18 @@ class LocalizationNode(Node):
         x_pose = msg.pose.pose.position.x
         y_pose = msg.pose.pose.position.y
         _, _, yaw = tft.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
-        self.odom_pos = np.array([[x_pose, y_pose, yaw]]).T
+        self.odom_pose = np.array([[x_pose, y_pose, yaw]]).T
         
-    def ekf_step(self):
-        u = get_odometry_input(self.odom_pos[:,0], self.prev_pose[:,0])
-        self.ekf.predict(u=u)
-        self.prev_pose = self.odom_pos
-
         # Simulate measuring landmarks
         for lmark in self.landmarks:
             z = z_landmark(self.true_pose, lmark, self.std_rng, self.std_brg, self.max_range, self.fov_deg)
             if z is not None:
                 self.ekf.update(z[:,0], lmark, residual=residual)
+        
+    def ekf_step(self):
+        u = get_odometry_input(self.odom_pose[:,0], self.prev_pose[:,0])
+        self.ekf.predict(u=u)
+        self.prev_pose = self.odom_pose.copy()
 
         self.get_logger().info('Pose: ' + str(self.ekf.mu[:,0]))
 
@@ -113,7 +113,6 @@ class LocalizationNode(Node):
         self.__ekf_pub.publish(msg)
 
 
-        
 
 
 def main(args=None):
