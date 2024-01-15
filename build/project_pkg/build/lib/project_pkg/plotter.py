6efaed7b60aa4a51df03b6a3f012cd6d15533859
KristofferMoevik <kristoffersmovik@live.no>
2024-01-15 13:21:12 +0000

    plotted different setups

--- build/project_pkg/build/lib/project_pkg/plotter.py
+++ build/project_pkg/build/lib/project_pkg/plotter.py
@@ -1,9 +1,8 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-import json
-
-from project_pkg.localization_node import initial_pose #, Mt, Qt, 
+import json 
 
 import yaml
 
@@ -14,6 +13,8 @@ file_path = '/workspaces/SESASR-project/src/project_pkg/config/localization_para
 with open(file_path, 'r') as file:
     data = yaml.safe_load(file)
 
+
+
 # Accessing parameters
 localization_node_params = data['localization_node']['ros__parameters']
 ekf_period_s = localization_node_params['ekf_period_s']
@@ -22,6 +23,16 @@ Qt_1 = localization_node_params['Qt_1']
 Mt_0 = localization_node_params['Mt_0']
 Mt_1 = localization_node_params['Mt_1']
 Mt_2 = localization_node_params['Mt_2']
+initial_pose = localization_node_params['initial_pose']
+std_rot1 = localization_node_params["std_rot1"] 
+std_transl = localization_node_params["std_transl"]
+std_rot2 = localization_node_params["std_rot2"]
+std_rng = localization_node_params["std_rng"]
+std_brg = localization_node_params["std_brg"]
+max_range = localization_node_params["max_range"]
+fov_deg = localization_node_params["fov_deg"]
+
+
 
 Qt = np.diag([Qt_0, Qt_1]) # measurement noise
 Mt = np.diag([Mt_0, Mt_1, Mt_2]) # motion noise
@@ -31,29 +42,29 @@ filter = np.load("filter.npy")
 odom = np.load("odom.npy")
 ground_truth = np.load("ground_truth.npy")
 
-# Plot odom data
-plt.scatter(odom[:, 0], odom[:, 1], label="odom")
-plt.legend()
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("Odom Data")
-plt.show()
+# # Plot odom data
+# plt.scatter(odom[:, 0], odom[:, 1], label="odom")
+# plt.legend()
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.title("Odom Data")
+# plt.show()
 
-# Plot ground truth data
-plt.scatter(ground_truth[:, 0], ground_truth[:, 1], label="ground truth")
-plt.legend()
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("Ground Truth Data")
-plt.show()
+# # Plot ground truth data
+# plt.scatter(ground_truth[:, 0], ground_truth[:, 1], label="ground truth")
+# plt.legend()
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.title("Ground Truth Data")
+# plt.show()
 
-# Plot filter data
-plt.scatter(filter[:, 0], filter[:, 1], label="filter")
-plt.legend()
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("Filter Data")
-plt.show()
+# # Plot filter data
+# plt.scatter(filter[:, 0], filter[:, 1], label="filter")
+# plt.legend()
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.title("Filter Data")
+# plt.show()
 
 
 # adjust data size to fit each other
@@ -78,8 +89,15 @@ orientation_rmse = math.sqrt(np.mean((filter[:,2] - ground_truth[:,2])**2))
 orientation_max_abs_error = np.max(np.abs(filter[:,2] - ground_truth[:,2]))
 orientation_total_cumulative_error = np.sum(np.abs(filter[:,2] - ground_truth[:,2]))
 
+# where to put results
+subfolder = "std_rot1:" + str(std_rot1) + "  std_transl:" + str(std_transl) + "  std_rot2:" + str(std_rot2) + "  std_rng:" + str(std_rng) + "  std_brg:" + str(std_brg) + "  max_range:" + str(max_range) + "  fov_deg:" + str(fov_deg) + "/"
+
+directory = "plots/" + subfolder
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
 # write the values to a .json file
-filename = f"results_Qt_{Qt.diagonal()}_Mt_{Mt.diagonal()}.json"
+filename = directory + f"results_Qt_{Qt.diagonal()}_Mt_{Mt.diagonal()}.json"
 with open(filename, 'a') as f:
     json.dump({
         "Qt diagonal": Qt.diagonal().tolist(),
@@ -91,7 +109,15 @@ with open(filename, 'a') as f:
         "position_error_percentage": position_error_percentage,
         "orientation_rmse": orientation_rmse,
         "orientation_max_abs_error": orientation_max_abs_error,
-        "orientation_total_cumulative_error": orientation_total_cumulative_error
+        "orientation_total_cumulative_error": orientation_total_cumulative_error,
+        "std_rot1": std_rot1, 
+        "std_transl": std_transl,
+        "std_rot2": std_rot2,
+        "std_rng": std_rng,
+        "std_brg": std_brg,
+        "max_range": max_range,
+        "fov_deg": fov_deg
+
     }, 
     f,
     indent=4
@@ -106,17 +132,18 @@ odom_offset[:, 0] -= 2
 odom_offset[:, 1] -= 0.5
 
 
+
 # plot 
 plt.scatter(filter[:,0], filter[:,1], label="filter")
-plt.scatter(odom_offset[:,0], odom_offset[:,1], label="odom")
-plt.scatter(ground_truth[:,0], ground_truth[:,1], label="ground truth")
+plt.plot(odom_offset[:,0], odom_offset[:,1], label="odom")
+plt.plot(ground_truth[:,0], ground_truth[:,1], label="ground truth")
 plt.scatter(landmarks[:,0], landmarks[:,1], color='red', label="landmarks")  # Add this line to plot the landmarks
 plt.legend()
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Position")
 filename = f"position_Qt_{Qt.diagonal()}_Mt_{Mt.diagonal()}.png"
-plt.savefig("plots/" + filename)
+plt.savefig(directory + filename)
 plt.close()
 
 plt.plot(np.sqrt((filter[:,0] - ground_truth[:,0])**2 + (filter[:,1] - ground_truth[:,1])**2))
@@ -124,7 +151,7 @@ plt.title("Position error")
 plt.xlabel("time")
 plt.ylabel("error")
 filename = f"position_error_Qt_{Qt.diagonal()}_Mt_{Mt.diagonal()}.png"
-plt.savefig("plots/" + filename)
+plt.savefig(directory + filename)
 plt.close()
 
 plt.plot(np.abs(filter[:,2] - ground_truth[:,2]))
@@ -132,4 +159,4 @@ plt.title("Orientation error")
 plt.xlabel("time")
 plt.ylabel("error")
 filename = f"orientation_error_Qt_{Qt.diagonal()}_Mt_{Mt.diagonal()}.png"
-plt.savefig("plots/" + filename)
+plt.savefig(directory + filename)
